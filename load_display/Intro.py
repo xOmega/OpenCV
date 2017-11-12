a9fd3084c16e527f733dbdb79a90750381cd57ce
Neeraj Kulkarni <nee.rajkul@yahoo.co.in>
2017-11-11 21:00:47 -0800

    Added imwrite function

--- load_display/Intro.py
+++ load_display/Intro.py
@@ -1,12 +1,17 @@
-# Basic Load and display code Using cv2 functions imread, imshow
+# Basic Load and display and copy code Using cv2 functions imread, imshow and imwrite
 
 import cv2
+import os
 
-img = cv2.imread("test.jpg", 0)
+
+parent = os.pardir
+img_file = os.path.join(parent, "test.jpg")
+# print img_file
+img = cv2.imread(img_file, 0)
 cv2.namedWindow("Demo")
-cv2.resizeWindow("Demo", 300, 300)
+# cv2.resizeWindow("Demo", 300, 300)
 cv2.imshow("Demo", img)
-
+cv2.imwrite("replica.jpg", img)
 
 k = cv2.waitKey(0)
 
