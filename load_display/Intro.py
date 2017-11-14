b2cb9b8d3e396371857c357842c7f1d25a1d7764
Neeraj Kulkarni <nee.rajkul@yahoo.co.in>
2017-11-14 01:12:58 -0800

    Added Notes and lessons learned

--- load_display/Intro.py
+++ load_display/Intro.py
@@ -1,4 +1,11 @@
 # Basic Load and display and copy code Using cv2 functions imread, imshow and imwrite
+# --------------------------------------------------------------------------------------------------------------
+# Notes:
+# 1. Color image loaded by OpenCV is in BGR mode. But Matplotlib displays in RGB mode. So color images will
+# not be displayed correctly in Matplotlib if image is read with OpenCV. Please see the exercises for more details.
+# 2. If you are using a 64-bit machine, you will have to modify k = cv2.waitKey(0) line as follows :
+# cv2.waitKey(0) & 0xFF
+
 
 import cv2
 import os
@@ -6,16 +13,13 @@ import os
 
 parent = os.pardir
 img_file = os.path.join(parent, "test.jpg")
-# print img_file
 img = cv2.imread(img_file, 0)
 cv2.namedWindow("Demo")
-# cv2.resizeWindow("Demo", 300, 300)
 cv2.imshow("Demo", img)
-cv2.imwrite("replica.jpg", img)
-
-k = cv2.waitKey(0)
+k = cv2.waitKey(0) & 0xFF
 
 if k == 27:
     cv2.destroyAllWindows()
-
-
+elif k == ord('s'):
+    cv2.imwrite("replica.jpg", img)
+    cv2.destroyAllWindows()
