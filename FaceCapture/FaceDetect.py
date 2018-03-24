94f4b436c2c568797c3dc4013732cdd4f24c5f52
xOmega <nee.rajkul@yahoo.co.in>
2018-03-23 20:42:25 -0700

    restructured Functions

--- FaceCapture/FaceDetect.py
+++ FaceCapture/FaceDetect.py
@@ -1,35 +1,50 @@
 import cv2
 # import numpy as np
 
-c = cv2.VideoCapture(0)  # 0 is the index of the camera
-face_cascade = cv2.CascadeClassifier("Haarcascade-Frontalface.xml")
 
-# fourcc = cv2.cv.FOURCC('M', 'J', 'P', 'G')
-# out = cv2.VideoWriter("test.mkv", fourcc, 23, (704, 400))
+def faceDetect(sourceIndex):
+    """
+    This is the first block in the facial recognition pipeline.
+    :param sourceIndex: Index of input video source. 0 is the index of the webcam
+    :return:
+    """
+    s = sourceIndex
+    c = cv2.VideoCapture(s)
+    face_cascade = cv2.CascadeClassifier("Haarcascade-Frontalface.xml")
 
-while c.isOpened():
-    ret, frame = c.read()
+    # fourcc = cv2.cv.FOURCC('M', 'J', 'P', 'G')
+    # out = cv2.VideoWriter("test.mkv", fourcc, 23, (704, 400))
 
-    if ret:
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(frame,
-                                              scaleFactor=1.2,
-                                              minNeighbors=5,
-                                              minSize=(30, 30),
-                                              flags=cv2.CASCADE_SCALE_IMAGE)
+    while c.isOpened():
+        ret, frame = c.read()
 
-        # out.write(frame)
-        if len(faces) > 0:
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        if ret:
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(frame,
+                                                  scaleFactor=1.2,
+                                                  minNeighbors=5,
+                                                  minSize=(30, 30),
+                                                  flags=cv2.CASCADE_SCALE_IMAGE)
 
-        cv2.imshow("f", frame)
+            # out.write(frame)
+            if len(faces) > 0:
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
-        if cv2.waitKey(1) & 0xFF == ord('q') or cv2.waitKey(1) & 0xFF == ord('Q'):
-            break
-    else:
-        break  # if videoCapture object failed to read the frame in c.read(), break
-c.release()
-# out.release()
-cv2.destroyAllWindows()
+            cv2.imshow("frame1", frame)
 
+            if cv2.waitKey(1) & 0xFF == ord('q') or cv2.waitKey(1) & 0xFF == ord('Q'):
+                break
+        else:
+            break  # if videoCapture object failed to read the frame in c.read(), break
+    c.release()
+    # out.release()
+    cv2.destroyAllWindows()
+
+
+def main():
+    faceDetect(0)
+
+
+if __name__ == '__main__':
+    main()
