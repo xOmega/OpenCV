3be9dedf6183f13fb9df9b1581e89a9a31224a50
xOmega <nee.rajkul@yahoo.co.in>
2018-04-11 17:35:07 -0700

    added eye detection

--- FaceCapture/FaceDetect.py
+++ FaceCapture/FaceDetect.py
@@ -1,35 +1,57 @@
 import cv2
 # import numpy as np
 
-c = cv2.VideoCapture(0)  # 0 is the index of the camera
-face_cascade = cv2.CascadeClassifier("Haarcascade-Frontalface.xml")
 
-# fourcc = cv2.cv.FOURCC('M', 'J', 'P', 'G')
-# out = cv2.VideoWriter("test.mkv", fourcc, 23, (704, 400))
+def faceDetect(sourceIndex):
+    """
+    This is the first block in the facial recognition pipeline.
+    :param sourceIndex: Index of input video source. 0 is the index of the webcam
+    :return:
+    """
+    s = sourceIndex
+    c = cv2.VideoCapture(s)
+    face_cascade = cv2.CascadeClassifier("Haarcascade-Frontalface.xml")
+    eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 
-while c.isOpened():
-    ret, frame = c.read()
+    # fourcc = cv2.cv.FOURCC('M', 'J', 'P', 'G')
+    # out = cv2.VideoWriter("test.mkv", fourcc, 23, (704, 400))
 
-    if ret:
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(frame,
-                                              scaleFactor=1.2,
-                                              minNeighbors=5,
-                                              minSize=(30, 30),
-                                              flags=cv2.CASCADE_SCALE_IMAGE)
+    while c.isOpened():
+        ret, frame = c.read()
 
-        # out.write(frame)
-        if len(faces) > 0:
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        if ret:
+            maxNumberOfFaces = 5
+            grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(grey_frame,
+                                                  scaleFactor=1.2,
+                                                  minNeighbors=maxNumberOfFaces,
+                                                  minSize=(30, 30),
+                                                  flags=cv2.CASCADE_SCALE_IMAGE)
 
-        cv2.imshow("f", frame)
+            # out.write(frame)
+            if len(faces) > 0:
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                    face_grey = grey_frame[y: y+h, x: x+w]
+                    face_color = frame[y: y+h, x: x+w]
+                    eyes = eye_cascade.detectMultiScale(face_grey)
+                    for (ex, ey, ew, eh) in eyes:
+                        cv2.rectangle(face_color, (ex, ey), (ew, eh), (0, 0, 255), 2)
 
-        if cv2.waitKey(1) & 0xFF == ord('q') or cv2.waitKey(1) & 0xFF == ord('Q'):
-            break
-    else:
-        break  # if videoCapture object failed to read the frame in c.read(), break
-c.release()
-# out.release()
-cv2.destroyAllWindows()
+            cv2.imshow("frame1", frame)
 
+            if cv2.waitKey(1) & 0xFF == ord('q') or cv2.waitKey(1) & 0xFF == ord('Q'):
+                break
+        else:
+            break  # if videoCapture object failed to read the frame in c.read(), break
+    c.release()
+    # out.release()
+    cv2.destroyAllWindows()
+
+
+def main():
+    faceDetect(0)
+
+
+if __name__ == '__main__':
+    main()
